6440fedb1b0a4bf9491138f095372fa1a70ce086
Sarah Schaller <sarcsch@aol.com>
2017-10-12 17:13:13 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     @user = User.find_by(username: params["username"])
     if @user != nil && @user.password == params[:password]
-      session[:id] = @user.id 
+      session[:id] = @user.id
       redirect to '/account'
-    end 
+    end
     erb :error
   end
 
@@ -29,4 +29,3 @@ class ApplicationController < Sinatra::Base
 
 
 end
-
