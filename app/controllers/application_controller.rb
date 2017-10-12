17f917a3e1bb2043f7de3dc2214b5893752c15ce
Sarah Schaller <sarcsch@aol.com>
2017-10-12 17:36:08 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,23 +11,23 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    @user = User.find_by(username: params[:username], password: params[:password])
-    if @user.nil?
-      erb :error
-    else
+    @user = User.find_by(username: params["username"], password: params["password"])
+    if @user != nil
       session[:user_id] = @user.id
       redirect to '/account'
+    else
+      erb :error
     end
   end
 
   get '/account' do
-
+    
 
   end
 
   get '/logout' do
     session.clear
-    redirect to '/'
+    redirect '/'
   end
 
 
